16584d2b92e4ca794484abae8c5529e7562c60ff
Syrgak-2022 <syrgakimankulov@gmail.com>
2022-12-13 11:53:56 +0300

    Add 3rd task of Homework #2

--- Homework002/002.py
+++ Homework002/002.py
@@ -43,3 +43,27 @@
 #     sum += i
 #
 # print("Сумма всех чисел последовательности:", sum)
+
+
+
+
+
+# 3. Реализуйте алгоритм перемешивания списка. Встроенный алгоритм SHUFFLE не использовать! Реализовать свой метод
+
+# from time import time
+# lister = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+#
+# def mix_list(old: list) -> list:
+#     new = []
+#     while old:
+#         x = str(time()).split('.')[1]
+#         x = list(map(int, [x[0], x[-1]]))
+#         x = x[0] if x[0] <= x[1] else x[1]
+#         if x > len(old)-1:
+#             new.append(old.pop(0))
+#         else:
+#             new.append(old.pop(x))
+#     return new
+#
+# lister = mix_list(lister)
+# print(lister)
