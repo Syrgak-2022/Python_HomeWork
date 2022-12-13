e27799e9ca7f6cec901ebd6e07e937a377f86817
Syrgak-2022 <syrgakimankulov@gmail.com>
2022-12-13 11:32:11 +0300

    Add 3rd task

--- Homework001/001.py
+++ Homework001/001.py
@@ -53,3 +53,44 @@
 # else:
 #     print(f"Утверждение ложно")
 
+
+
+
+
+# 3. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и
+# выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+# Пример:
+# x=34; y=-30 -> 4
+# x=2; y=4 -> 1
+# x=-34; y=-30 -> 3
+
+# def inputCoordinates(x):
+#     a = [0] * x
+#     for i in range(x):
+#         is_Ok = False
+#         while not is_Ok:
+#             try:
+#                 number = float(input(f"Введите {i+1} координату: "))
+#                 a[i] = number
+#                 is_Ok = True
+#                 if a[i] == 0:
+#                     is_Ok = False
+#                     print("Координата не должно быть равна 0 ")
+#             except ValueError:
+#                 print("Ты ошибся. Вводить надо числа!")
+#     return a
+#
+# def checkCoordinates(xy):
+#     text = 4
+#     if xy[0] > 0 and xy[1] > 0:
+#         text = 1
+#     elif xy[0] < 0 and xy[1] > 0:
+#         text = 2
+#     elif xy[0] < 0 and xy[1] < 0:
+#         text = 3
+#     print(f"Точка находится в {text} четверти плоскости")
+#
+# Coordinate = inputCoordinates(2)
+# checkCoordinates(Coordinate)
+
+
